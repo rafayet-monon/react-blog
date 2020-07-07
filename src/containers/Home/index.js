310bdec4d6ea6e8479015ed3270940729d55bafb
rafayet <rafayet@hashkloud.com>
2020-07-07 15:36:45 +0600

    Refactored SCSS file to better structure

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./style.css";
 import backgroundImage from "../../images/home-bg.jpg";
 import Header from "../../components/Header";
 import BlogList from "../../components/BlogList";
